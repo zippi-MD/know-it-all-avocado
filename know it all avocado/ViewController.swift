6a1d2e0615ce106f7f15a4e8a4a26dd95f7d24cd
Alejandro Mendoza <zippi180@gmail.com>
2018-05-15 15:32:39 -0500

    Initial Commit

---
+++ know it all avocado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  know it all avocado
+//
+//  Created by Alejandro Mendoza on 5/15/18.
+//  Copyright © 2018 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
