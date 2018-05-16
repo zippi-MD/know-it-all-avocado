dbca6a409d16bbb52dc3828505e55e692b59ffe5
Alejandro Mendoza <zippi180@gmail.com>
2018-05-16 00:39:53 -0500

    Added a way to show random phrases when asked a question

--- know it all avocado/ViewController.swift
+++ know it all avocado/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 import AlertOnboarding
 
+let avocadoNames: [String] = ["The Mighty Avocado Says:", "The Know It All Avocado Says:", "Super Avocado Says:"]
+let avocadoPhrases: [String] = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "You can count on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Absolutely", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", "Chances aren't good", "No"]
+
 class ViewController: UIViewController, AlertOnboardingDelegate  {
 
     override func viewDidLoad() {
@@ -17,8 +20,11 @@ class ViewController: UIViewController, AlertOnboardingDelegate  {
     }
     
     @IBAction func makeQuestion(_ sender: Any) {
-        print("Make a question button pressed...")
-        let alertView = AlertOnboarding(arrayOfImage: ["Octocat"], arrayOfTitle: ["OctoCat"], arrayOfDescription: ["Description"])
+        let phrase = avocadoPhrases[Int(arc4random_uniform(UInt32(avocadoPhrases.count)))]
+        let name = avocadoNames[Int(arc4random_uniform(UInt32(avocadoNames.count)))]
+        let alertView = AlertOnboarding(arrayOfImage: ["Octocat"], arrayOfTitle: [name], arrayOfDescription: [phrase])
+        alertView.percentageRatioHeight = 1.0
+        alertView.percentageRatioWidth = 1.0
         alertView.show()
     }
 
